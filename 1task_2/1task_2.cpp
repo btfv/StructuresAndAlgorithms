49f478c08c0ae3193171e17bfa9a7c54a8ff87cc
bulat faizrakhmanov <fayzrakhman7@gmail.com>
2021-03-22 14:20:12 +0300

    Исправлена реализация стека через массив. Теперь работает через шаблон класса.

--- 1task_2/1task_2.cpp
+++ 1task_2/1task_2.cpp
@@ -1,28 +1,55 @@
 ﻿#include <iostream>
+#include <string>
 #include "array_stack.h"
-#include "list_stack.h"
 
 int main()
 {
     setlocale(LC_CTYPE, "Russian");
 
-    std::cout << "Реализация через список:\n";
-    list_stack* lstack = new list_stack();
-    lstack->push_back(1);
-    lstack->push_back(2);
-    lstack->push_back(3);
-    for (int i = 0; i < 3; i++) {
-        std::cout << lstack->get_root()->getValue() << "\n";
-        lstack->pop_back();
-    }
-
     std::cout << "Реализация через массив:\n";
-    array_stack* stack = new array_stack();
-    stack->push_back(1);
-    stack->push_back(2);
-    stack->push_back(3);
-    for (int i = 0; i < 3; i++) {
-        std::cout << stack->back() << "\n";
-        stack->pop_back();
+    array_stack<int> stack;
+    std::string s;
+    std::cin >> s;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '[') {
+            stack.push_back((int)'[');
+        }
+        else if (s[i] == ']') {
+            if (stack.back() != (int)'[') {
+                std::cout << "Строка составлена неправильно!\n";
+                return 0;
+            }
+            stack.pop_back();
+        }
+
+        if (s[i] == '<') {
+            stack.push_back((int)'<');
+        }
+        else if (s[i] == '>') {
+            if (stack.back() != (int)'<') {
+                std::cout << "Строка составлена неправильно!\n";
+                return 0;
+            }
+            stack.pop_back();
+        }
+
+        if (s[i] == '(') {
+            stack.push_back((int)'(');
+        }
+        else if (s[i] == ')') {
+            if (stack.back() != (int)'(') {
+                std::cout << "Строка составлена неправильно!\n";
+                return 0;
+            }
+            stack.pop_back();
+        }
     }
+    if (!stack.is_empty()) {
+        std::cout << "Строка составлена неправильно!\n";
+        return 0;
+    }
+    std::cout << "Строка составлена правильно!\n";
+    
+    return 0;
 }
--- 1task_2/array_stack.h
+++ 1task_2/array_stack.h
@@ -1,19 +1,70 @@
 #pragma once
-
+template <typename T>
 class array_stack {
 private:
-	int* begin;
+	T* begin;
 	int elements_count;
 	int array_size;
 	const int default_size = 10;
-
 	void resize(const int new_size);
 public:
 	array_stack();
 	array_stack(const int size);
-	int back();
+	T back();
 	bool is_empty();
 	void pop_back();
-	void push_back(int element);
+	void push_back(T element);
 	~array_stack();
 };
+
+template <typename T>
+void array_stack<T>::resize(const int new_size) {
+	if (new_size <= elements_count) {
+		return;
+	}
+	int* new_begin = new T[new_size];
+	for (int i = 0; i < elements_count; i++) {
+		new_begin[i] = begin[i];
+	}
+	delete[] begin;
+	begin = new_begin;
+	array_size = new_size;
+}
+
+template <typename T>
+array_stack<T>::array_stack() {
+	begin = new T[default_size];
+	elements_count = 0;
+	array_size = default_size;
+}
+
+template <typename T>
+array_stack<T>::array_stack(const int size) {
+	begin = new T[size];
+	this->array_size = size;
+	elements_count = 0;
+}
+template <typename T>
+T array_stack<T>::back() {
+	return *(begin + elements_count - 1);
+}
+template <typename T>
+bool array_stack<T>::is_empty() {
+	return elements_count == 0;
+}
+template <typename T>
+void array_stack<T>::pop_back() {
+	elements_count--;
+}
+template <typename T>
+void array_stack<T>::push_back(T element) {
+	if (elements_count >= array_size) {
+		resize(array_size + default_size);
+	}
+	begin[elements_count] = element;
+	elements_count++;
+}
+template <typename T>
+array_stack<T>::~array_stack() {
+	delete[] begin;
+}
